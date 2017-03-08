a54f112a335b9c4f029e249e28bf637d958bf2ad
Karl Grossner <karl.geog@gmail.com>
2017-03-07 19:27:11 -0700

    owtrad push

--- _site/data/hashes.js
+++ _site/data/hashes.js
@@ -37,9 +37,9 @@ window.tlConfig = {
 }
 blurbs = {
   "courier":"Ming Dynasty Courier Routes, 14-17c CE",
-  "incanto-f":"Venetian Incanto Trade Flows, 13-15c CE",
+  "incanto":"Venetian Incanto Trade Flows, 13-15c CE",
   "roundabout": "@kgeographer perambulation, 2016-17",
   "vicarello": "Vicarello Beaker #1, 1-3c CE",
   "xuanzang": "Pilgrimage of Xuanzang, 7c CE",
-  "owtrad": "Old World Trade Routes (OWTRAD), 2c BCE - 19c CE",
+  "owtrad": "Old World Trade Routes (OWTRAD), 2c BCE-19c CE",
 }
