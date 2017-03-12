fc2998b912865b926780d3418682fcfed2a19d9b
Karl Grossner <karl.geog@gmail.com>
2017-03-11 18:44:13 -0700

    distinct ids

--- py/csvToGeoJSON-T.py
+++ py/csvToGeoJSON-T.py
@@ -11,8 +11,8 @@ def init():
     #os.chdir('./py')
     global proj, reader_p, reader_s, finp, fins, fout, foutp, fouts, collection, collectionAttributes, routeidx
     # owtrad, courier, incanto-f, incanto-j, roundabout, vicarello, xuanzang
-    proj = 'roundabout'
-    data = 'roundabout'
+    proj = 'courier'
+    data = 'courier'
 
     finp = codecs.open('../data/source/'+proj+'/places_'+proj+'.csv', 'r', 'utf8')
     fins = codecs.open('../data/source/'+proj+'/segments_'+data+'.csv', 'r', 'utf8')
