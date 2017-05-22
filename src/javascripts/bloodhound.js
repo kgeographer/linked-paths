f9499c5938cf6f6270f9f9283c62227b4da6e183
Karl Grossner <karl.geog@gmail.com>
2017-05-22 16:43:43 -0600

    connections returned for each related project

--- src/javascripts/bloodhound.js
+++ src/javascripts/bloodhound.js
@@ -20,76 +20,9 @@ var years = function(timespan){
   }
   return str;
 }
-// segmentSearch = function(obj){
-//   console.log('segmentSearch obj', obj)
-//   ga('send', 'event', ['Search'], ['Select'], ['Search panel']);
-//   // retrieve all segments associated with a place,
-//   // populate results_inset
-//   let html = ''
-//   var plKeys = Object.keys(obj)
-//   window.relevantProjects = []
-//   // console.log('plKeys', plKeys)
-//   for(let i = 0; i < plKeys.length; i++){
-//     // console.log('plKeys[i]', plKeys[i])
-//     relevantProjects.push(obj[plKeys[i]][0])
-//     // console.log('search source and target for:', plKeys[i])
-//     // multi_match
-//     var searchParams = {
-//       index: 'linkedplaces',
-//       type: 'segment',
-//       body: {
-//         query: {
-//           multi_match : {
-//               query : plKeys[i],
-//               fields: ["properties.source","properties.target"]
-//           }
-//         }
-//       }
-//     }
-//
-//     esclient.search(searchParams).then(function (resp) {
-//       return Promise.all(resp.hits.hits)
-//     }).then(function(hitsArray){
-//         console.log('hitsArray', hitsArray)
-//         html += '<div class="place-card">'+
-//           '<p class="search-result-project">from: <em>'+obj[plKeys[i]][0]+'</em></p>'+
-//           '<h4><a href="#" project="'+obj[plKeys[i]][0]+
-//           '" id="'+plKeys[i]+'">'+obj[plKeys[i]][1]+
-//           '</a> connections:</h4><ul class="ul-segments">';
-//         for(let j = 0; j < hitsArray.length; j++){
-//           let l = hitsArray[j]._source.properties.label
-//           html += '<li>'+(l==''?'<em>no label</em>':l)+' ('+
-//           years(hitsArray[j]._source.when.timespan)+
-//           ')</li>';
-//         };
-//         html += '</ul></div>'
-//         $("#results_inset").html(html)
-//         $(".place-card a").click(function(e){
-//           ga('send', 'event', ['Search'], ['Choose dataset'], ['Search panel']);
-//           window.proj = $(this).attr('project')
-//           // console.log('project',proj)
-//           // if project/dataset isn't loaded, load it (project !- dataset for incanto)
-//           window.pcheck = $("input:checkbox[value='"+proj+"']")
-//           // console.log('toponym checked',pcheck,proj)
-//           if(pcheck.prop('checked') == false){
-//             // loadLayer(proj,idToFeature[proj].places[this.id])
-//             location.href = location.origin+location.pathname+'?d='+proj+'&p='+this.id
-//             pcheck.prop('checked', true)
-//           } else {
-//             // console.log(proj,'already loaded, zoom to',this.id)
-//           }
-//           // console.log('got data, now place', this.id)
-//           // ttmap.setView(idToFeature[proj].places[this.id].getLatLng(),6)
-//           // idToFeature[proj].places[this.id].openPopup()
-//         })
-//       }).catch(console.log.bind(console));
-//   }
-//   // clear map layers if present, load relevant datasets
-//   // loadLayers(relevantProjects);
-// }
 
 window.segmentSearch = function(obj){
-  console.log('segmentSearch obj', obj)
+  // console.log('segmentSearch obj', obj)
   ga('send', 'event', ['Search'], ['Select'], ['Search panel']);
   // retrieve all segments associated with a place,
   // populate results_inset
@@ -99,7 +32,7 @@ window.segmentSearch = function(obj){
   for(let i = 0; i < plKeys.length; i++){
     // console.log('plKeys[i]', obj[plKeys[i]])
     for(let j=0;j<obj[plKeys[i]].length;j++) {
-      console.log('j in i',obj[plKeys[i]][j])
+      // console.log('j in i',obj[plKeys[i]][j])
       relevantProjects.push(obj[plKeys[i]][j][0])
       // multi_match an id in segment sources and targets
       var searchParams = {
@@ -122,8 +55,8 @@ window.segmentSearch = function(obj){
           // console.log('hitsArray', hitsArray)
           html += '<div class="place-card">'+
             // '<p class="search-result-project">from: <em>'+obj[plKeys[i]][j][0]+'</em></p>'+
-            '<h4><a href="#" project="'+obj[plKeys[i]][0]+
-            '" id="'+plKeys[i]+'">'+obj[plKeys[i]][j][0]+
+            '<h4><a href="#" project="'+obj[plKeys[i]][j][0]+
+            '" id="'+obj[plKeys[i]][j][2]+'">'+obj[plKeys[i]][j][0]+
             '</a> connections:</h4><ul class="ul-segments">';
           for(let j = 0; j < hitsArray.length; j++){
             let l = hitsArray[j]._source.properties.label
@@ -147,18 +80,10 @@ window.segmentSearch = function(obj){
             } else {
               // console.log(proj,'already loaded, zoom to',this.id)
             }
-            // console.log('got data, now place', this.id)
-            // ttmap.setView(idToFeature[proj].places[this.id].getLatLng(),6)
-            // idToFeature[proj].places[this.id].openPopup()
           })
         }).catch(console.log.bind(console));
-
     }
-    console.log(relevantProjects)
-
   }
-  // clear map layers if present, load relevant datasets
-  // loadLayers(relevantProjects);
 }
 
 // resolve collection names as they exist in data
