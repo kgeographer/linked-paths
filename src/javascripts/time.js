a7a4789554d94e6f614a70f187ec450fb2af7fee
Karl Grossner <karl.geog@gmail.com>
2017-05-23 15:59:39 -0600

    tabs bare beginning

--- src/javascripts/time.js
+++ src/javascripts/time.js
@@ -1,6 +1,27 @@
 // visjs.org -> vis-timeline-graph2d.min.js
 var d3 = Object.assign({}, require("d3"), require("d3-scale"));
 
+window.openTab = function(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
 window.makeTimeVis = function(periodArray,dataset,pid){
   console.log('dataset,pid',dataset, pid)
   var newdiv = document.createElement('div')
